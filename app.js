74425c3720083f350b75bf53a4ba640d46407aff
Sarah E <seatherly.prsvr@gmail.com>
2023-12-13 14:38:11 -0600

    edit

--- app.js
+++ app.js
@@ -5,13 +5,28 @@ const logger = require('morgan')
 require('dotenv').config()
 
 app.use(cors())
-app.use(express.json())
 app.use(logger('dev'))
+app.use(express.urlencoded({extended: true}))
 
 
 const mongoose = require('mongoose')
 
-const User = require('./model/User')
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    username: String
+})
+
+const User = mongoose.model('User', UserSchema)
+
+const ExerciseSchema = new Schema({
+    user_id: {type: String, required: true},
+    description: String,
+    duration: Number,
+    date: Date
+})
+
+const Exercise = mongoose.model('Exercise', ExerciseSchema)
 
 
 mongoose
@@ -30,14 +45,16 @@ app.get('/', (req, res) => {
   });
 
 app.post('/api/users', async (req, res)=>{
-   const {username} = req.body
-   let newUser = await User.findOne({username: username}).exec();
-   if(newUser !== null){
-    res.json({username: newUser.username, _id: newUser._id})
-   }else{
-    newUser = new User({username: username})
-    await newUser.save()
-    res.json({username: newUser.username, _id: newUser._id})
+   const userObject = new User({
+    username: req.body.username
+   })
+   
+   try {
+    const user = await userObject.save()
+    console.log(user);
+    res.json(user)
+   } catch (error) {
+    console.log(error);
    }
 })
 
@@ -53,16 +70,32 @@ app.get('/api/users', async (req, res)=>{
 })
 
 app.post('/api/users/:_id/exercises', async (req, res)=>{
-    let {duration, description, date} = req.body;
-    if(!date){
-        let realDate = new Date()
-    }else{
-        let realDate = new Date(date)
+    const {id} = req.params._id
+    const {description, duration, date} = req.body
+    try {
+        const user = await User.findById(id)
+        if(!user){
+            res.send('Could not find user.')
+        }else{
+            const exerciseObj = new Exercise({
+                user_id: user._id,
+                description,
+                duration,
+                date: date ? new Date(date) : new Date()
+            })
+            const exercise = await exerciseObj.save()
+            res.json({
+                _id: user._id,
+                username: user.username,
+                description: exercise.description,
+                duration: exercise.duration,
+                date: new Date(exercise.date).toDateString()
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.send('there was an error')
     }
-    let numberDur = parseInt(duration)
-    let updatedUser = await User.findByIdAndUpdate({_id: req.params._id}, {description: description, duration: numberDur, date: realDate}, {new: true,
-    runValidators: true})
-    res.json({username, description, duration, date, _id})
 })
 
 app.get('/api/users/:_id/logs', async (req, res)=>{
